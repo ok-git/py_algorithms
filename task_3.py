14d26082682eeb8fb85c58f33e8a623f531d5f12
ok-git <okmail.inbox@gmail.com>
2020-04-13 10:45:19 +0700

    Готово task_3

--- task_3.py
+++ task_3.py
@@ -11,9 +11,15 @@ array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 result = []
 print(array)
 
-min_val, min_idx = 0, 0
-max_val, max_idx = 0, 0
+min_val, min_idx = array[0], 0
+max_val, max_idx = array[0], 0
 
-for el in enumerate(array):
-    pass
-
+for idx, el in enumerate(array):
+    if el < min_val:
+        min_val = el
+        min_idx = idx
+    if el > max_val:
+        max_val = el
+        max_idx = idx
+array[min_idx], array[max_idx] = array[max_idx], array[min_idx]
+print(array)
