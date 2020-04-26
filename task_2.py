7989ae193f431aef4bae571c434626b37328f54f
ok-git <okmail.inbox@gmail.com>
2020-04-27 01:57:26 +0700

    В работе task_2 итеративный метод

--- task_2.py
+++ task_2.py
@@ -6,7 +6,19 @@
 import random
 
 
-# Адаптация алгоритма простого двухпутевого слияния на псевдокоде с Википедии на Python
+# Адаптация для Python алгоритма простого двухпутевого слияния на псевдокоде с Википедии
+def rec_merge_sort(array):
+    if len(array) == 1:
+        return array
+    middle = len(array) // 2
+    left = array[:middle]
+    right = array[middle:]
+    left = rec_merge_sort(left)
+    right = rec_merge_sort(right)
+    result = merge(left, right)
+    return result
+
+
 def merge(left, right):
     result = []
     while len(left) > 0 and len(right) > 0:
@@ -26,18 +38,31 @@ def merge(left, right):
 
 
 def merge_sort(array):
-    if len(array) == 1:
-        return array
-    middle = len(array) // 2
-    left = array[:middle]
-    right = array[middle:]
-    left = merge_sort(left)
-    right = merge_sort(right)
-    result = merge(left, right)
-    return result
+    for chunk in range(1, len(array) // 2):
+        result = []
+        for i in range(0, len(array), chunk):
+            left = array[i:chunk]
+            right = array[i + chunk:i + chunk + chunk]
+            while len(left) > 0 and len(right) > 0:
+                if left[0] <= right[0]:
+                    result.append(left[0])
+                    left = left[1:]
+                else:
+                    result.append(right[0])
+                    right = right[1:]
+            while len(left) > 0:
+                result.append(left[0])
+                left = left[1:]
+            while len(right) > 0:
+                result.append(right[0])
+                right = right[1:]
+        array = result
+    return array
 
 
 array = [random.uniform(0, 49) for _ in range(10)]
 print(array)
-
+print(rec_merge_sort(array))
+array = [random.uniform(0, 49) for _ in range(6)]
+print(array)
 print(merge_sort(array))
