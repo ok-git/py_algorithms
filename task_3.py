bf4cbb04e23515b0956119cd123ace2e4e42b94c
ok-git <okmail.inbox@gmail.com>
2020-04-27 18:44:52 +0700

    Готово task_3

--- task_3.py
+++ task_3.py
@@ -6,26 +6,33 @@
 Примечание: задачу можно решить без сортировки исходного массива. Но если это слишком сложно, используйте метод
 сортировки, который не рассматривался на уроках
 """
+
 import random
 
 m = 5
 array = [random.randint(0, 99) for _ in range(2 * m + 1)]
 print(array)
 
-array = [45, 32, 53, 45, 62, 64, 56, 58, 74, 30, 13]
-print(array)
-print([13, 30, 32, 45, 45, 53, 56, 58, 62, 64, 74])
-
-median = None
-median_counter = len(array) // 2
-for i in array:
-    el_less_counter = 0
-    el_more_counter = 0
-    for j in array:
-        if i < j:
-            el_less_counter += 1
-        if i > j:
-            el_more_counter += 1
-    if el_more_counter == median_counter and el_less_counter == median_counter:
+median = None  # Значение медианы
+median_counter = len(array) // 2  # Позиция медианы для данного массива
+for i in array:  # Каждый элемент массива...
+    i_less_counter = 0  # сколько раз i-ый элемент меньше, чем другие элементы массива
+    i_more_counter = 0  # сколько раз i-ый элемент больше, чем другие элементы массива
+    for j in array:  # ...сравниваем с каждым элементом массива
+        if i is not j:  # кроме него самого
+            if i < j:
+                i_less_counter += 1
+            elif i > j:
+                i_more_counter += 1
+            else:  # если элемент равен i-ому, то плюсуем оба счётчика, что бы сошлось общее кол-во с median_counter
+                i_less_counter += 1
+                i_more_counter += 1
+    """
+    Если хотя бы один из счётчиков больше/меньше для i-го элемента равен эталонному median_counter,
+    значит мы уже достигли медианы и выходим из цикла
+    """
+    if i_more_counter == median_counter or i_less_counter == median_counter:
         median = i
+        break
 print(median)
+print(sorted(array))
