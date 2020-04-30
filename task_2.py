ff902e7f9ba261ca8be7a82eba9a89964d67cda9
ok-git <okmail.inbox@gmail.com>
2020-05-01 01:44:29 +0700

    В работе task_2

--- task_2.py
+++ task_2.py
@@ -2,9 +2,53 @@
 Закодируйте любую строку по алгоритму Хаффмана.
 """
 import collections
+from tree_model import Tree
+from tree_model import Node
+
+class HuffmanNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        # return f'Node[{self.value:^5}]'
+        return f"Node {self.value} - left {self.left} - right {self.right}"
+
+    def __str__(self):
+        return f"{self.value} - {self.left} - {self.right}"
+
 
 string = "beep boop beer!"
-
 symbols_counter = collections.Counter(string)
+# Counter({'e': 4, 'b': 3, 'p': 2, ' ': 2, 'o': 2, 'r': 1, '!': 1})
+
+# ----------------------------------------------------------------------------------------
+#                                        Node[  8  ]
+#                 Node[  4  ]                                 Node[ 12  ]
+#      Node[  2  ]            Node[  6  ]          Node[ 10  ]            Node[ 14  ]
+# Node[  0  ]Node[  3  ]Node[  5  ]Node[  7  ]Node[  9  ]Node[ 11  ]Node[ 13  ]Node[ 15  ]
+# ----------------------------------------------------------------------------------------
+
+t = Tree()
+while len(symbols_counter) > 1:
+    key_1, val_1 = symbols_counter.popitem()
+    key_2, val_2 = symbols_counter.popitem()
+    t.root = t.new_node(val_1 + val_2)
+    t.root.left = t.new_node(key_1)
+    t.root.right = t.new_node(key_2)
+    symbols_counter.update({t.root: val_1 + val_2})
+    # symbols_counter[t.root] = val_1 + val_2
+
+# while len(symbols_counter) > 1:
+#     key_1, val_1 = symbols_counter.popitem()
+#     key_2, val_2 = symbols_counter.popitem()
+#     node = HuffmanNode('')
+#     node.left = HuffmanNode(key_1)
+#     node.right = HuffmanNode(key_2)
+#     symbols_counter[node] = val_1 + val_2
+
+t.print_level_order(t.root)
 
 print(symbols_counter)
+print(len(symbols_counter))
---
+++ tree_model.py
@@ -0,0 +1,112 @@
+class Node:
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f'Node[{self.value:^5}]'
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    # функция для добавления узла в дерево
+    def new_node(self, value):
+        return Node(value, None, None)
+
+    # функция для вычисления высоты дерева
+    def height(self, node):
+        if node is None:
+            return 0
+        else:
+            left_height = self.height(node.left)
+            right_height = self.height(node.right)
+
+            if left_height > right_height:
+                return left_height + 1
+            else:
+                return right_height + 1
+
+    # функция для распечатки элементов на определенном уровне дерева
+    def print_given_level(self, root, level):
+        if root is None:
+            return
+        if level == 1:
+            print(root, end='')
+        elif level > 1:
+            self.print_given_level(root.left, level - 1)
+            self.print_given_level(root.right, level - 1)
+
+    # функция для распечатки дерева
+    def print_level_order(self, root):
+        h = self.height(root)
+        i = 1
+        while i <= h:
+            self.print_given_level(self.root, i)
+            print()
+            i += 1
+
+    # функция для вычисления ширины дерева
+    def get_max_width(self, root):
+        max_wdth = 0
+        i = 1
+        h = self.height(root)
+        while i <= h:
+            width = self.get_width(root, i)
+            if width > max_wdth:
+                max_wdth = width
+            i += 1
+
+        return max_wdth
+
+    def get_width(self, root, level):
+        if root is None:
+            return 0
+        if level == 1:
+            return 1
+        elif level > 1:
+            return self.get_width(root.left, level - 1) + self.get_width(root.right, level - 1)
+        self.get_width(root.right, level - 1)
+
+    def search(self, root, number, path=''):
+        if root.value == number:
+            return f'Число {number} найдено по следующему пути:\nКорень{path}'
+        if root.value > number and root.left is not None:
+            return self.search(root.left, number, path=f'{path}\nШаг влево')
+        if root.value < number and root.right is not None:
+            return self.search(root.right, number, path=f'{path}\nШаг вправо')
+        return f'Число {number} отсутствует в дереве'
+
+
+if __name__ == '__main__':
+    t = Tree()
+    t.root = t.new_node(8)
+    t.root.left = t.new_node(4)
+    t.root.right = t.new_node(12)
+    t.root.left.left = t.new_node(2)
+    t.root.left.right = t.new_node(6)
+    t.root.right.left = t.new_node(10)
+    t.root.right.right = t.new_node(14)
+    t.root.left.left.left = t.new_node(0)
+    t.root.left.left.right = t.new_node(3)
+    t.root.left.right.left = t.new_node(5)
+    t.root.left.right.right = t.new_node(7)
+    t.root.right.left.left = t.new_node(9)
+    t.root.right.left.right = t.new_node(11)
+    t.root.right.right.left = t.new_node(13)
+    t.root.right.right.right = t.new_node(15)
+
+    t.print_level_order(t.root)
+    print(f'высота: {t.height(t.root)}')
+    print(f'ширина: {t.get_max_width(t.root)}')
+
+# ----------------------------------------------------------------------------------------
+#                                        Node[  8  ]
+#                 Node[  4  ]                                 Node[ 12  ]
+#      Node[  2  ]            Node[  6  ]          Node[ 10  ]            Node[ 14  ]
+# Node[  0  ]Node[  3  ]Node[  5  ]Node[  7  ]Node[  9  ]Node[ 11  ]Node[ 13  ]Node[ 15  ]
+# ----------------------------------------------------------------------------------------
+
+    print(t.search(t.root, 9))
